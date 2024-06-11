5d8abddceb2b10ce00ff789a11573967476f4d39
Vbrawl <greninjagr@gmail.com>
2024-06-12 00:06:51 +0300

    Modified gruntfile to include testing.

--- gruntfile.js
+++ gruntfile.js
@@ -1,3 +1,6 @@
+const path = require("node:path");
+const fs = require("node:fs/promises");
+
 module.exports = function(grunt) {
     grunt.initConfig({
         ts: {
@@ -9,17 +12,69 @@ module.exports = function(grunt) {
             test: {
                 files: [{
                     expand: false,
-                    cwd: "tests/html",
-                    src: ["**/*.html"],
-                    dest: "tests/",
-                    ext: ".super-html"
+                    cwd: "tests/sources",
+                    src: ["**/*.html", "!**/_*.html"],
+                    dest: "tests/actual_results",
+                    ext: ".html"
                 }]
             },
+        },
+
+        file_compare: {
+            test: {
+                options: {
+                    expected_results_folder: "tests/expected_results",
+                    actual_results_folder: "tests/actual_results"
+                }
+            }
         }
     });
 
     grunt.loadTasks("tasks");
 
     grunt.loadNpmTasks("grunt-ts");
+
+
+    grunt.registerTask("test", ["super_html_compiler", "file_compare"]);
     grunt.registerTask("default", ["ts"]);
+
+
+    grunt.registerMultiTask("file_compare", "Compare all files under actual_results_folder and expected_results_folder", function() {
+        var done = this.async();
+        var options = this.options({
+            expected_results_folder: undefined,
+            actual_results_folder: undefined
+        });
+
+        if(!options.expected_results_folder || !grunt.file.isDir(options.expected_results_folder)) {
+            grunt.fail.fatal("expected_results_folder needs to point to a directory.")
+        }
+        if(!options.actual_results_folder || !grunt.file.isDir(options.actual_results_folder)) {
+            grunt.fail.fatal("actual_results_folder needs to point to a directory.")
+        }
+        
+        (async function(options) {
+            const ar_files = await fs.readdir(options.actual_results_folder);
+            const er_files = await fs.readdir(options.expected_results_folder);
+
+            for (let i = 0; i < er_files.length; i++) {
+                const file_name = er_files[i];
+                const ar_i = ar_files.findIndex((v) => {return v === file_name});
+                if(ar_i === -1) {
+                    grunt.log.warn(`${path.basename(file_name)}: Doesn't exist in ${options.expected_results_folder}.`);
+                    continue;
+                }
+
+                const expected_file_path = path.join(options.expected_results_folder, file_name);
+                const actual_file_path = path.join(options.actual_results_folder, file_name);
+
+                const ef = await fs.readFile(expected_file_path);
+                const af = await fs.readFile(actual_file_path);
+
+                if(ef.equals(af))   { grunt.log.write("[+]"["green"]); }
+                else                { grunt.log.write("[-]"["red"]); }
+                grunt.log.writeln(`${expected_file_path} == ${actual_file_path}`);
+            }
+        }(options)).then(done);
+    });
 }
